d768e825e18086739a1722faf115c154327b15af
Karsh Patel <93849481+Karsh01@users.noreply.github.com>
2024-07-30 15:24:23 +1000

    Deployment using Streamlit

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+from xgboost import XGBClassifier
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('/Users/karsh/Downloads/ML_Projects/NLP-Sentiment_Analysis_Qantas_Airways/vectorizer.pkl','rb'))
+model = pickle.load(open('/Users/karsh/Downloads/ML_Projects/NLP-Sentiment_Analysis_Qantas_Airways/model.pkl','rb'))
+
+st.title("Qantas Airway Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
